3a5cd7a4069bf5067eb196be26a3f301b7e4a6fa
Daniel Orozco <daniel.orozco.jaramillo@gmail.com>
2026-07-18 12:21:56 -0500

    feat: dockerize FastAPI backend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.api.hello import router as hello_router
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hola FastAPI"}
+
+app.include_router(hello_router)
