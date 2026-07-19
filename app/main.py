26f5629e9bbaafa1b42b625e94d5a84272044139
Daniel Orozco <daniel.orozco.jaramillo@gmail.com>
2026-07-19 09:49:20 -0500

    Se habilita cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.hello import router as hello_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "https://fodec-fe.web.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
